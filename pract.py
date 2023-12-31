5f3f0fedcbfcc861ece6db25021304c717b0327d
Dana S <dana.s.lewis@gmail.com>
2023-12-31 10:21:55 -0500

    modifying pract.py

--- pract.py
+++ pract.py
@@ -1,3 +1,16 @@
 n1 = 1
 n = "{:02d}".format(n1)
 print(n)
+
+# practice with range and for loop
+for _ in range(5):
+    print(_)
+
+word = "camel"
+word_length = len(word)
+
+new_word = []
+for position in range(word_length):
+    letter = word[position]
+    new_word += letter
+print(f"{' '.join(new_word)}")
